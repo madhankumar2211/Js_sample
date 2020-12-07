348d9f09915df00a77cc570deea726e6f47f2931
madhankumar2211 <madhankumar199722@gmail.com>
2020-12-07 11:14:54 +0530

    sample

--- Sum and Product/sum.js
+++ Sum and Product/sum.js
@@ -1,1 +1,9 @@
-
+let arr = [1, 2, 3, 4, 5];
+let sum = 0, product = 1;
+for (let i = 0; i < arr.length; i += 1)
+{
+    sum += arr[i];
+    product *= arr[i];
+}
+console.log('Sum = '+ sum + '\t Product = ' + product )
+    
--- pattern/pattern.js
+++ pattern/pattern.js
@@ -1,1 +1,21 @@
+let str="";
+for (let i=1;i<=5;i++)
+{
+    for(let j=1;j<=i;j++)
+    {
+        document.write("*");
+        if(i==j)
+            continue;
 
+        document.write(" ");
+    }
+    document.write("<br>")
+  
+}
+
+//display O/P in console
+
+// for(let i = 1; i < 5; i++)
+// {
+//     console.log("*".repeat(i));
+// }
