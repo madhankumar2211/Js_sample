f0fd2346494d9ceaf5b6ea9f5c219f75a36eec8a
madhankumar2211 <madhankumar199722@gmail.com>
2020-12-07 19:16:03 +0530

    calc change

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,1 +1,31 @@
+let result;
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
 
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        // console.log(number1+"+"+number2+"="+result)
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
