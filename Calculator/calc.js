c99be3b5db056dba142b2c50072fbe30c3e0ccbb
madhankumar2211 <madhankumar199722@gmail.com>
2020-12-07 19:19:18 +0530

    CALCULATOR

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,7 +1,7 @@
 let result;
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
-const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const operator = prompt('Enter operator ( +, -, * or / ): ');
 
 switch(operator) {
     case '+':
