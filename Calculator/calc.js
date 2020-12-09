6de7daf38a7579c3b72226a831e4f4d6e54f2b8a
madhankumar2211 <madhankumar199722@gmail.com>
2020-12-09 10:56:04 +0530

    calculator

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,31 +1,14 @@
-let result;
-const number1 = parseFloat(prompt('Enter first number: '));
-const number2 = parseFloat(prompt('Enter second number: '));
-const operator = prompt('Enter operator ( +, -, * or / ): ');
+function num(n){
+    document.getElementById("result").value+=n;
+}
 
-switch(operator) {
-    case '+':
-         result = number1 + number2;
-        console.log(`${number1} + ${number2} = ${result}`);
-        // console.log(number1+"+"+number2+"="+result)
-        break;
+function res(){
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value=y;
+    
+}
 
-    case '-':
-         result = number1 - number2;
-        console.log(`${number1} - ${number2} = ${result}`);
-        break;
-
-    case '*':
-         result = number1 * number2;
-        console.log(`${number1} * ${number2} = ${result}`);
-        break;
-
-    case '/':
-         result = number1 / number2;
-        console.log(`${number1} / ${number2} = ${result}`);
-        break;
-
-    default:
-        console.log('Invalid operator');
-        break;
+function clr(){
+    document.getElementById("result").value="";
 }
